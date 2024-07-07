8e2925225eb367d8be4b51b9e44092f655c6ddd7
lonebhen <ben.angmortey@gmail.com>
2024-07-07 22:49:14 +0000

    nlp cache

--- model/models.py
+++ model/models.py
@@ -3,12 +3,6 @@ from enum import Enum
 
 db = SQLAlchemy()
 
-
-class NLPAPICache(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    api_key = db.Column(db.String(255), unique=True, nullable = False)
-    value = db.Column(db.String(255), nullable = False)
-    
 class DialectEnum(Enum):
     TWI = "tw"
     FANTE ="fat"
@@ -19,6 +13,15 @@ class DialectEnum(Enum):
     HAUSA = "hausa"
     EWE = "ee"
     
+
+
+class NLPAPICache(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    api_key = db.Column(db.String(255), unique=True, nullable = False)
+    value = db.Column(db.String(255), nullable = False)
+    local_dialect = db.Column(db.Enum(DialectEnum), nullable=False)
+    
+
     
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
